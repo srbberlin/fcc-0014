cc686c4aaabf63c2c8b206484a1c904ed161b19c
srbberlin <info@hans-schrieber.de>
2017-12-06 23:35:27 +0100

    First runnig version

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,76 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync');
-const sequence = require('run-sequence').use(gulp);
-const del = require('del');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const babel = require('gulp-babel')
+//const concat = require('gulp-concat')
+const browserSync = require('browser-sync')
+//bootconst sequence = require('gulp-series')
+const del = require('del')
 
 var config = {
-    base:   '/home/srb/src/FCC/repos/fcc-0014/',
-    html:   '**/*.html',
-    cssin:  'sass/**/*.sass',
-    cssout: 'css/',
-    jsin:   'babel/**/*.js',
-    jsout:  'js/',
-  };
+  base:   __dirname + '/',
+  html:   '**/*.html',
+  cssin:  'sass/**/*.sass',
+  cssout: 'css/',
+  jsin:   'babel/**/*.js',
+  jsout:  'js/',
+}
 
-  gulp.task('reload', function() {
-    browserSync.reload();
-  });
-  
-  gulp.task('serve', ['sass', 'babel'], function() {
-    browserSync({
-      server: config.base
-    });
-  
-    gulp.watch(config.html, ['reload']);
-    gulp.watch(config.jsin, ['babel', 'reload']);
-    gulp.watch(config.cssin, ['sass', 'reload']);
-  });
-  
-  gulp.task('sass', function() {
-    return gulp.src(config.base + config.scssin)
-      .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-      //.pipe(autoprefixer({
-      //  browsers: ['last 3 versions']
-      //}))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(config.base + config.cssout))
-      .pipe(browserSync.stream());
-  });
-  
-  gulp.task('babel', function() {
-    return gulp.src(config.base + config.jssin)
-      .pipe(babel(babel({
-        presets: ['env']
-      })))
-      .pipe(gulp.dest(config.base + config.jsout))
-      .pipe(browserSync.stream());
-  });
-  
-  gulp.task('build', function() {
-    sequence('clean', ['babel', 'sass']);
-  });
-  
-  gulp.task('clean', function() {
-    let res = del([config.base + config.js + '/*', config.base + config.css + '/*']);
-    res.then(function (a, b) {
-      console.log('clean', a, b);
-    });
-    return res;
-  });
+gulp.task('reload', function() {
+  console.log('')
+  browserSync.reload()
+})
 
-  gulp.task('default', ['serve']);
-  
+gulp.task('serve', ['sass', 'babel'], function() {
+  console.log('serve')
+  browserSync({
+    server: config.base
+  })
+
+  gulp.watch(config.html, ['reload'])
+  gulp.watch(config.jsin, ['babel', 'reload'])
+  gulp.watch(config.cssin, ['sass', 'reload'])
+})
+
+gulp.task('sass', function() {
+  let path = config.base + config.cssin
+  console.log('sass', path)
+  return gulp.src(path)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    //.pipe(autoprefixer({
+    //  browsers: ['last 3 versions']
+    //}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.base + config.cssout))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('babel', function() {
+  let path = config.base + config.jsin
+  console.log('babel', path)
+  return gulp.src(path)
+    .pipe(babel())
+    .pipe(gulp.dest(config.base + config.jsout))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('clean', function() {
+  let paths = [config.base + config.jsout + '/*.js', config.base + config.cssout + '/*.css']
+  let res;
+  console.log('clean', [config.base + config.js + '/*.js', config.base + config.css + '/*.css'])
+  res = del(paths)
+  res.then(function (a) {
+    console.log('clean', a)
+  }).catch( function (a) {
+    console.log('clean', a)
+  })
+  return res
+})
+
+gulp.task('build', ['babel', 'sass'])
+
+gulp.task('default', ['serve'])
+
   
   
