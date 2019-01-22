bd0ada81e77a7547a8328a0038ec957a8ff331c4
srbberlin <info@hans-schrieber.de>
2019-01-22 11:13:49 +0100

    Gulp version 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,66 @@
 const gulp = require('gulp')
-const runSeq = require('run-sequence')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 const babel = require('gulp-babel')
 const browserSync = require('browser-sync')
 
 var config = {
-  cssin:   __dirname + '/css/**/*.css',
-  jsin:    __dirname + '/babel/**/*.js',
-  htmlin:  __dirname + '/html/**/*.html',
-  cssout:  __dirname + '/docs/css/',
-  jsout:   __dirname + '/docs/js/',
-  htmlout: __dirname + '/docs/'
+  cssin:    __dirname + '/sass/**/*.sass',
+  jsin:     __dirname + '/babel/**/*.js',
+  htmlin:   __dirname + '/html/**/*.html',
+  assetsin: __dirname + '/assets/**/*',
+  cssout:   __dirname + '/docs/css/',
+  jsout:    __dirname + '/docs/js/',
+  htmlout:  __dirname + '/docs/'
 }
 
-gulp.task('reload', function () {
+function reload () {
   browserSync.reload()
-})
+}
 
-gulp.task('serve', ['html', 'sass', 'babel'], function () {
-  browserSync({
-    server: config.htmlout
-  })
-
-  gulp.watch(config.jsin, () => runSeq(['babel', 'reload']))
-  gulp.watch(config.cssin, () => runSeq(['sass', 'reload']))
-  gulp.watch(config.htmlin, () => runSeq(['html', 'reload']))
-})
-
-gulp.task('html', function () {
+function html () {
   let path = config.htmlin
   return gulp.src(path)
     .pipe(gulp.dest(config.htmlout))
-})
+}
 
-gulp.task('sass', function () {
+function assets () {
+  let path = config.assetsin
+  return gulp.src(path)
+    .pipe(gulp.dest(config.htmlout))
+}
+
+function css () {
   let path = config.cssin
   return gulp.src(path)
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.cssout))
-})
+}
 
-gulp.task('babel', function () {
+function babl () {
   let path = config.jsin
   return gulp.src(path)
     .pipe(babel().on('error', e => {
       console.log('babel', e)
     }))
     .pipe(gulp.dest(config.jsout))
-})
+}
 
-gulp.task('build', ['html', 'babel', 'sass'])
+function serve () {
+  browserSync({
+    server: config.htmlout
+  })
 
-gulp.task('default', ['serve'])
+  gulp.watch(config.jsin, () => gulp.series(babl, reload))
+  gulp.watch(config.cssin, () => gulp.series(css, reload))
+  gulp.watch(config.htmlin, () => gulp.series(html, reload))
+  gulp.watch(config.assetsin, () => gulp.series(assets, reload))
+}
+
+exports.build = function () {
+  gulp.series(build, gulp.parallel(html, assets, babel, sass))
+}
+
+exports.default = serve
